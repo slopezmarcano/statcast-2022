4f1f433c3aff416435785fccde5f1194b35e4a3c
Sebastian Lopez-Marcano <slopezmarcano@gmail.com>
2023-03-27 10:10:39 +1000

    advances iun 001_statcast_visualisations 1. treemap completed 2. boxplot of maximum distance 3. identify those with the biggest hits 4. country influence 5. still working on adding the hyperlinkof CJ Cron Homerun

--- scripts/001_statcast_visualisations.R
+++ scripts/001_statcast_visualisations.R
@@ -1,12 +1,15 @@
 #SLM
-#Created:
+#Created:20032023
 
 
 #--DESCRIPTION--#
+# a few visualisations and analysis of MLB statcast data
 
 #--LIBRARIES--#
 suppressPackageStartupMessages(library(tidyverse)) #data wrangling and management
-#suppressPackageStartupMessages(library(ggthemes)) #ggplot themes
+suppressPackageStartupMessages(library(treemap))
+suppressPackageStartupMessages(library(ggthemes)) #ggplot themes
+suppressPackageStartupMessages(library(ggiraph))
 #suppressPackageStartupMessages(library(ggtext)) #add and modify text to ggpplot
 #suppressPackageStartupMessages(library(showtext)) #fonts
 #font_add_google("Lato") #theme font
@@ -18,14 +21,14 @@ suppressPackageStartupMessages(library(tidyverse)) #data wrangling and managemen
 
 #--DATASET--#
 data <- read_csv('data/mlb_statcast_2022.csv')
-#data <- read_parquet('data/.parquet')
 
+#--DISRTIBUTION OF PLAYERS BY COUNTRY--#
+#counts
 counts <- data %>%
     group_by(country) %>%
     count()
-# Package
-library(treemap)
 
+#visualisation
 treemap(counts,
         index = "country",
         vSize = "n",
@@ -34,10 +37,36 @@ treemap(counts,
         fontsize.labels = 14,
         palette = palettoni)
 
-#--DATA WRANGLING--#
+#--MAXIMUM HOME RUN DISTANCE BY COUNTRY--#
+#get the player that hit the ball the furthest
+max_row <- data[which.max(data$max_distance),] %>%
+        mutate(link = "https://www.youtube.com/watch?v=kqklZHfQVjQ",
+                link_name = "MLB video")
+player_label <- paste(max_row$first_name, max_row$last_name)
+
+p<-data %>%
+  ggplot(aes(x=country, y=max_distance, fill=country)) +
+    geom_boxplot() +
+    scale_fill_manual(values=palettoni) +
+    geom_jitter(color="#000000dd", size=0.4, alpha=0.9) +
+    geom_text(data = subset(data, max_distance >480), aes(label=paste(last_name, first_name, attempts)), vjust =-1)+
+    geom_text(data = subset(data, max_distance <390), aes(label=paste(last_name, first_name, attempts)), vjust =-1)+
+    geom_point_interactive(max_row, aes(x=country, y=max_distance,
+                tooltip=paste0("<a href='", link, "'>", link_name, "</a>\n", link, onclick=paste0('window.open("', link , '")'))))+
+    labs(
+        x= "",
+        y = "Maximun Hit Distance (Yards)",
+        title = "StatCast Maximum Distance by Country in 2022",
+        subtitle = "",
+        caption = "Visualization by S Lopez Marcano • ") +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 
 
+
+girafe(ggobj=p,
+       height_svg = 5,
+       width_svg = 5)
  
 #--VISUALISATIONS--#
 plot <- ggplot(data, aes(x = , y = )) +
